a062a31190a1b59a4e5118da5310f58e35bf63f8
srihasa20 <90495112+srihasa20@users.noreply.github.com>
2023-05-13 01:00:04 +0530

    Update router.js

--- api/users/router.js
+++ api/users/router.js
@@ -1,14 +1,6 @@
 const { createUser, getUserByUserId, getUsers, updateUser, deleteUser, Login } = require("./controller")
 const router =require("express").Router()
 
-/*router.post("/", createUser)
-router.get("/", getUsers)
-router.get("/:id", getUserByUserId)
-router.patch("/", updateUser)
-router.delete("/", deleteUser)*/
 router.post("/login", Login)
-
-
-
 
 module.exports = router
