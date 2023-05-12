f023ec569ebb50e24b8ef8773668755dfd9519d1
srihasa20 <90495112+srihasa20@users.noreply.github.com>
2023-05-13 01:06:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.use(express.json())
 app.use("/api/users", userRouter)
 app.listen(process.env.APP_PORT, () =>
 {
-    console.log("Server up and running on PORT: ", process.env.APP_PORT)
+    console.log("Server running on PORT: ", process.env.APP_PORT)
 })
 
