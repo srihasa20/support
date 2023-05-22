ea6cfa9df3ea594770c151c8f8f607ed16eaf411
srihasa20 <90495112+srihasa20@users.noreply.github.com>
2023-05-22 22:35:24 +0530

    Update service.js

--- api/users/service.js
+++ api/users/service.js
@@ -1,21 +1,53 @@
-const pool = require("../../configuration/database")
+const {create, getUserByUserId, getUsers, updateUser, deleteUser,getUserByUserEmail } = require("./service")
+
+const{ genSaltSync, hashSync, compareSync} = require("bcrypt")
+
+const{ signin} = require("jsonwebtoken")
 
 
-
-module.exports = {
-getUserByUserEmail: (email, callback)=>
+module.exports ={
+Login: (req, res)=>
 {
-    pool.query(
-        'select * from registration where email = ?',
-        [email],
-        (error, results, fields)=> {
-            if(error)
-            {
-                return callback(error)
+    const body = req.body;
+    getUserByUserEmail(body.email, (err, results)=>
+    {
+        if(err)
+        {
+            console.log(err)
+        }
+
+        if(!results.length){
+            return res.json({
+                success: 0,
+                data: "Invalid email or password"
+            })
+        }
+
+        results = results[0]
+
+
+        const result = compareSync(body.password, results.password)
+
+
+        if(result)
+        {
+            results.password = undefined
+            const jsontoken = signin({result: results})
+            return res.json(
+                {
+                    success: 1,
+                    message:"login successfully",
+                    token: jsontoken
+                })
             }
-            return callback(null, results[0])
-        })
-    
-    }
-    
+            else{
+                return res.json({
+                    success: 0,
+                    data: "Invalid email or passwords"
+                })
+            }
+    })
+}
+
+
 }
